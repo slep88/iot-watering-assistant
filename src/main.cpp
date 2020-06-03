cfdd33a73185275db9fcfdbea69f09211efb2b19
slep88 <slep88@googlemail.com>
2020-06-03 21:46:23 +0200

    Initial version of MQTT publisher

---
+++ src/main.cpp
@@ -0,0 +1,60 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+#include <config.h> 
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+long lastMsg = 0;
+char msg[50];
+int value = 0;
+
+void setup_wifi() {
+    delay(10);
+    Serial.println();
+    Serial.print("Connecting to ");
+    Serial.println(SSID);
+ 
+    WiFi.begin(SSID, PSK);
+ 
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+ 
+    Serial.println("");
+    Serial.println("WiFi connected");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void setup() {
+    Serial.begin(115200);
+    setup_wifi();
+    client.setServer(MQTT_BROKER, 1883);
+}
+ 
+
+void reconnect() {
+    while (!client.connected()) {
+        Serial.print("Reconnecting...");
+        if (!client.connect("ESP8266Client")) {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" retrying in 5 seconds");
+            delay(5000);
+        }
+    }
+}
+void loop() {
+ 
+    if (!client.connected()) {
+        reconnect();
+    }
+    client.loop();
+ 
+    snprintf (msg, 50, "Alive since %ld milliseconds", millis());
+    Serial.print("Publish message: ");
+    Serial.println(msg);
+    client.publish("/home/data", "Hello World");
+    delay(5000);
+}
